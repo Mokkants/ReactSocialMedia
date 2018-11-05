848246f3d0d5b41d4b162ff679a3a59a0ae1c108
Bence Ludmány <mokkants@gmail.com>
2018-11-05 01:06:18 +0100

    Add config folder and move relevant stuff there

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 "use strict";
 import mongoose from 'mongoose'
 import app from './express'
+import config from './../config/config'
 
-//TODO move these to some config file
-let port = process.env.PORT || 3000;
-let mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/app';
-
-mongoose.connect(mongoURI, {useNewUrlParser: true}, function(err){
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoURI, {useNewUrlParser: true}, function(err){
   if (err){
     console.error(`Failed to connect to MongoDB with URI ${mongoURI}`);
     console.error(err.stack);
@@ -14,15 +12,13 @@ mongoose.connect(mongoURI, {useNewUrlParser: true}, function(err){
   }
 });
 
-let db = mongoose.connection;
+mongoose.connection.once('open', function() {
+    app.listen(config.port);
+    console.log("Server started on port %s.", port);
+  });
 
 //Basic error handling TODO probably do something nice instead of this
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send('Something broke!')
-});
-
-db.once('open', function() {
-  app.listen(port);
-  console.log("Server started on port %s.", port);
 });
