b0e6e8037ac6d75d69d4c4d68e7e4ec6986b2cd6
Bence Ludmány <mokkants@gmail.com>
2018-11-07 00:17:30 +0100

    Create home view

---
+++ client/core/Home.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+    card: {
+      maxWidth: 600,
+      margin: 'auto',
+      marginTop: theme.spacing.unit * 5
+    },
+    title: {
+      padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px 
+      ${theme.spacing.unit * 2}px`,
+      color: theme.palette.text.secondary
+    },
+    media: {
+      minHeight: 400
+    }
+}) 
+
+class Home extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+        appName:"[INSERT SOCIAL MEDIA NAME HERE]"
+      }
+    }
+    render() {
+      const {classes} = this.props 
+      return (
+        <div>
+          <Card className={classes.card}>
+            <Typography type="headline" component="h1" className=
+            {classes.title}>
+              Home
+            </Typography>
+            <CardContent>
+              <Typography type="body2" component="h2">
+                Welcome to {this.state.appName}!
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      )
+    }
+}
+
+Home.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Home)
