ae7b53cb2c59ff12da34d956598f36c4362397ce
Bence Ludmány <mokkants@gmail.com>
2018-11-10 00:10:16 +0100

    Add text input to chat component

--- client/pages/Chat.js
+++ client/pages/Chat.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Messages from './chat/Messages';
+import InputBox from './chat/InputBox';
 
 class Chat extends Component{
     constructor(){
@@ -10,14 +11,21 @@ class Chat extends Component{
                 name:"Filip"
             }
         }
+        this.sendMessage = this.sendMessage.bind(this)
     }
     render(){
-        return( 
+        return(<div>
             <Messages 
                 messages={this.state.messages} 
                 partner={this.state.partner}
             />
-        )
+            <InputBox sendMessage={this.sendMessage}/>
+        </div>)
+    }
+    sendMessage(text){
+        let messages = this.state.messages;
+        messages.push({senderName:"Filip", text:text});
+        this.setState({messages:messages})
     }
 }
 
