bbe52f5ae24ad58600758708ae597dc12fcbdb91
Bence Ludmány <mokkants@gmail.com>
2018-11-10 00:42:13 +0100

    Add header to chat component

--- client/pages/Chat.js
+++ client/pages/Chat.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Messages from './chat/Messages';
 import InputBox from './chat/InputBox';
+import Header from './chat/Header'
 
 class Chat extends Component{
     constructor(){
@@ -8,13 +9,14 @@ class Chat extends Component{
         this.state={
             messages:[{senderName:"Filip",text:"Hi!"}, {senderName:"Filip",text:"Hi!"}, {senderName:"Filip",text:"Hi!"}],
             partner:{
-                name:"Filip"
+                name:"George"
             }
         }
         this.sendMessage = this.sendMessage.bind(this)
     }
     render(){
         return(<div>
+            <Header partner={this.state.partner}/>
             <Messages 
                 messages={this.state.messages} 
                 partner={this.state.partner}
