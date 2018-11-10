7d63486c2682212e96c80e335ead4a8e4311e937
Bence Ludmány <mokkants@gmail.com>
2018-11-10 01:36:50 +0100

    Update conditional render

--- client/pages/chat/Messages.js
+++ client/pages/chat/Messages.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Message from './messages/Message';
 
-const Messages = (props) => {
-    if(!props.messages){
-        return <div>Start a conversation with {props.partner.name}</div>
-    }
-    else{
-        return (
-            <ul className="messages">
-                {props.messages.map(message=>{
-                    return <Message message={message}/>
-                })}
-            </ul>
-        )
-    } 
+const Messages = ({messages,partner}) => {
+    return (
+        <ul className="messages">
+        {messages ? (
+            <div>Start a conversation with {partner.name}!</div> 
+        ) : (
+            messages.map(message=>{
+                return <Message message={message}/>
+            })
+        )}      
+        </ul>
+    )
 }
 
 export default Messages
