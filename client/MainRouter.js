dd12b4375a0eca7754c9419449156addaebeee3e
Bence Ludmány <mokkants@gmail.com>
2018-11-10 23:27:47 +0100

    Menu added, margin/padding reset at template, rm unnecessary newline in Home

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './pages/Home'
+import Menu from './core/Menu'
 
 class MainRouter extends Component {
   render() {
     return (<div>
+      <Menu/>
       <Switch>
         <Route exact path="/" component={Home}/>
       </Switch>
