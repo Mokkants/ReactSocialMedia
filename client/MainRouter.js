f995d8a3fd634ef4ebc080a81510ba4e75c5f505
Bence Ludmány <mokkants@gmail.com>
2018-11-11 23:52:30 +0100

    Add login and signup routes to router

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './pages/Home'
+import SignUp from './pages/SignUp';
 
 class MainRouter extends Component {
   render() {
     return (<div>
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={SignUp}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/login" component={Signin}/>
       </Switch>
     </div>)
   }
